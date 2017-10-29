d3ee6f3338f08e2d8b2e29d34acf384b555df9ed
markozeman <mz4730@student.uni-lj.si>
2017-10-29 18:43:07 +0100

    Initial commit

---
+++ preprocessing.R
@@ -0,0 +1,7 @@
+
+IDs_mapping <- read.table('dataset_diabetes/IDs_mapping.csv', header = TRUE, sep = ',');
+
+diabetes <- read.table('dataset_diabetes/diabetic_data.csv', header = TRUE, sep = ',');
+
+
+
