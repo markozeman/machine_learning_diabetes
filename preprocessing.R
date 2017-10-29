a5e58f6463d8e2334759677a79e83a2dda788e9a
markozeman <mz4730@student.uni-lj.si>
2017-10-29 18:46:58 +0100

    Testing commit

--- preprocessing.R
+++ preprocessing.R
@@ -3,5 +3,5 @@ IDs_mapping <- read.table('dataset_diabetes/IDs_mapping.csv', header = TRUE, sep
 
 diabetes <- read.table('dataset_diabetes/diabetic_data.csv', header = TRUE, sep = ',');
 
-
+a <- c(1,2,3);
 
