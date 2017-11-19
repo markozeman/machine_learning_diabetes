4e537bd43b351c453850ffd560d2540feee0663c
markozeman <mz4730@student.uni-lj.si>
2017-11-19 19:56:52 +0100

    Each row is different patient

--- preprocessing.R
+++ preprocessing.R
@@ -1,7 +1,12 @@
 
 IDs_mapping <- read.table('dataset_diabetes/IDs_mapping.csv', header = TRUE, sep = ',');
-
 diabetes <- read.table('dataset_diabetes/diabetic_data.csv', header = TRUE, sep = ',');
 
-a <- c(1,2,3);
+# check if all encounter ids are different
+length(unique(diabetes[, 1])) == length(diabetes[, 1]);
+
+# make rows unique according to patient number (so each row is different patient)
+diabetes_unique = diabetes[!duplicated(diabetes[, c("patient_nbr")]), ];
+
+
 
